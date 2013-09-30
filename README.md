62e77a73f3c03863c4bb914351efb903f9a1e079
Simon Fairbairn <simon@line-in.co.uk>
2013-09-30 19:37:58 +0200

    Adding a much more useful animation

--- README.md
+++ README.md
@@ -5,6 +5,4 @@ An iOS 7 location icon drawn in code and Storyboard-ready. Takes its colour from
 
 Simply draw out a square view and it'll do the rest.
 
-Animates into a circle because CODING CHALLENGE. Probably don't use that.
-
-(Or fork it and make it animate into something more useful.)
+Animates by scaling and rotating into an activity indicator mainly just as a coding exercise. 
--- Source/VTALILocationIconView.m
+++ Source/VTALILocationIconView.m
@@ -15,6 +15,9 @@ const CGFloat offset = 5.0f;
 @property (nonatomic, strong) CAShapeLayer *shapeLayer;
 @property (nonatomic) BOOL isCircle;
 
+@property (nonatomic, strong) UIActivityIndicatorView *activity;
+
+@property (nonatomic, strong) UIImageView *iconImage;
 
 @end
 
@@ -24,41 +27,57 @@ const CGFloat offset = 5.0f;
 #pragma mark - Properties
 
 -(CAShapeLayer *)shapeLayer {
+
     if ( !_shapeLayer ) {
-        _shapeLayer = [CAShapeLayer layer];
         
+        _shapeLayer = [CAShapeLayer layer];
         _shapeLayer.frame = self.bounds;
-
         _shapeLayer.path = [[self drawTriangle] CGPath];
-
         _shapeLayer.fillColor = self.tintColor.CGColor;
         _shapeLayer.lineWidth = 1.0f;
         _shapeLayer.lineJoin = kCALineJoinBevel;
+
     }
     return _shapeLayer;
 }
 
+-(UIActivityIndicatorView *)activity {
+    if ( !_activity ) {
+        _activity = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        _activity.hidesWhenStopped = YES;
+        _activity.alpha = 0.0f;
+        _activity.frame = CGRectMake(CGRectGetMidX(self.bounds) - _activity.frame.size.width / 2, CGRectGetMidY(self.bounds) - _activity.frame.size.height / 2, _activity.frame.size.width, _activity.frame.size.height);
+    }
+    return _activity;
+}
+
 #pragma mark - Initialisation
 
-- (id)initWithFrame:(CGRect)frame
-{
+- (id)initWithFrame:(CGRect)frame{
+    
     self = [super initWithFrame:frame];
+    
     if (self) {
         // Initialization code
         [self setup];
     }
+    
     return self;
 }
 
 -(id)initWithCoder:(NSCoder *)aDecoder {
+    
     self = [super initWithCoder:aDecoder];
+    
     if ( self ) {
         [self setup];
     }
+    
     return self;
 }
 
 -(void)setup {
+    [self addSubview:self.activity];
     [self.layer addSublayer:self.shapeLayer];
 }
 
@@ -80,8 +99,6 @@ const CGFloat offset = 5.0f;
     
     UIBezierPath *path = [UIBezierPath bezierPath];
     
-    
-    
     CGFloat twoThirdsDown = floor(CGRectGetMidY(self.bounds) );
     CGFloat twoThirdsAcross = floor( CGRectGetMidX(self.bounds) );
     
@@ -99,7 +116,6 @@ const CGFloat offset = 5.0f;
     
     [path addLineToPoint:CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2)];
     [path closePath];
-
     
 //    self.shapeLayer.path = [self drawTriangle].CGPath;
     return path;
@@ -108,19 +124,141 @@ const CGFloat offset = 5.0f;
 - (UIBezierPath *)drawCircle {
     
     CGRect newBounds = CGRectInset(self.bounds, offset, offset);
-    
     UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:newBounds];
-    
-    
-    
     //    self.shapeLayer.path = [self drawTriangle].CGPath;
     return path;
+    
 }
 
 
 
-- (void)animate
-{
+- (void)animate {
+    
+    CGFloat duration = 0.5;
+    
+    CATransform3D firstFrame;
+    CATransform3D secondFrame;
+    CATransform3D thirdFrame;
+    CATransform3D fourthFrame;
+    CATransform3D fifthFrame;
+    
+    if ( CATransform3DIsIdentity(self.shapeLayer.transform)) {
+        firstFrame  = CATransform3DMakeScale(1.0f, 1.0f, 1.0);
+        
+        secondFrame = CATransform3DMakeScale(0.75f, 0.75f, 1.0); // halfway to 1.0 from 0.75
+        secondFrame = CATransform3DRotate(secondFrame, M_PI_2, 0, 0, 1);
+        
+        thirdFrame = CATransform3DMakeScale(0.5f, 0.5f, 1.0); // halfway to 1.0 from 0.75
+        thirdFrame= CATransform3DRotate(thirdFrame, M_PI, 0, 0, 1);
+        
+        fourthFrame = CATransform3DMakeScale(0.25f, 0.25f, 1.0); // halfway to 1.0 from 0.75
+        fourthFrame = CATransform3DRotate(fourthFrame, M_PI_2 * 3, 0, 0, 1);
+        
+        fifthFrame = CATransform3DMakeScale(0.0f, 0.0f, 1.0); // halfway to 1.0 from 0.75
+        fifthFrame = CATransform3DRotate(fifthFrame, 0, 0, 0, 1);
+        
+        [self.activity startAnimating];
+        [UIView animateWithDuration:duration animations:^{
+            self.activity.alpha = 1.0f;
+        }];
+        
+        
+    } else {
+        firstFrame  = CATransform3DMakeScale(0.0f, 0.0f, 1.0);
+        
+        secondFrame = CATransform3DMakeScale(0.25f, 0.25f, 1.0); // halfway to 1.0 from 0.75
+        secondFrame = CATransform3DRotate(secondFrame, M_PI_2, 0, 0, 1);
+        
+        thirdFrame = CATransform3DMakeScale(0.5f, 0.5f, 1.0); // halfway to 1.0 from 0.75
+        thirdFrame= CATransform3DRotate(thirdFrame, M_PI, 0, 0, 1);
+        
+        fourthFrame = CATransform3DMakeScale(0.75f, 0.75f, 1.0); // halfway to 1.0 from 0.75
+        fourthFrame = CATransform3DRotate(fourthFrame, M_PI_2 * 3, 0, 0, 1);
+        
+        fifthFrame = CATransform3DMakeScale(1.0f, 1.0f, 1.0); // halfway to 1.0 from 0.75
+        fifthFrame = CATransform3DRotate(fifthFrame, 0, 0, 0, 1);
+        
+
+        [UIView animateWithDuration:duration animations:^{
+            self.activity.alpha = 0.0f;
+        }];
+
+        
+    }
+    
+    CAKeyframeAnimation *scaleAndRotate = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    scaleAndRotate.values = @[[NSValue valueWithCATransform3D:firstFrame],
+                              [NSValue valueWithCATransform3D:secondFrame],
+                              [NSValue valueWithCATransform3D:thirdFrame],
+                              [NSValue valueWithCATransform3D:fourthFrame],
+                              [NSValue valueWithCATransform3D:fifthFrame]];
+    scaleAndRotate.duration = duration;
+    scaleAndRotate.timingFunctions = @[[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn],
+                                       [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
+                                       [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
+                                       [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
+                                       [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut]];
+    scaleAndRotate.delegate = self;
+    scaleAndRotate.removedOnCompletion = NO;
+    scaleAndRotate.fillMode = kCAFillModeForwards;
+    
+    [self.shapeLayer addAnimation:scaleAndRotate forKey:@"entire animation"];
+    
+    return;
+    
+    // Other animations and experiments
+    
+    
+    duration = 1.0f;
+
+    CAKeyframeAnimation* animation;
+    animation = [CAKeyframeAnimation animationWithKeyPath:@"transform.rotation.z"];
+    animation.repeatCount = 1;
+    animation.removedOnCompletion = NO;
+    animation.fillMode = kCAFillModeForwards;
+    
+    animation.values = [NSArray arrayWithObjects:
+                            [NSNumber numberWithFloat: [[self.shapeLayer valueForKeyPath:@"transform.rotation.z"] floatValue]],
+                            [NSNumber numberWithFloat:M_PI * 2],
+                            nil];
+    animation.keyTimes = [NSArray arrayWithObjects:
+                          [NSNumber numberWithFloat:0.0],
+                          [NSNumber numberWithFloat:duration], nil];
+    
+    animation.timingFunctions = [NSArray arrayWithObjects:
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut], nil];
+    
+    
+
+    
+    CAKeyframeAnimation *scaleAnim = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnim.removedOnCompletion = NO;
+    scaleAnim.fillMode = kCAFillModeForwards;
+    scaleAnim.removedOnCompletion = NO;
+    scaleAnim.fillMode = kCAFillModeForwards;
+
+    
+    scaleAnim.values = [NSArray arrayWithObjects:
+                        [NSNumber numberWithFloat: 1.0f],
+                        [NSNumber numberWithFloat:0.0f],
+                        nil];
+    scaleAnim.keyTimes = [NSArray arrayWithObjects:
+                          [NSNumber numberWithFloat:0.0],
+                          [NSNumber numberWithFloat:duration], nil];
+    
+    scaleAnim.timingFunctions = [NSArray arrayWithObjects:
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut], nil];
+    
+    CAAnimationGroup *animationGroup = [[CAAnimationGroup alloc] init];
+    animationGroup.animations = @[animation, scaleAnim];
+    animationGroup.duration = duration;
+    
+    [self.shapeLayer addAnimation:animationGroup forKey:@"Group"];
+    self.shapeLayer.transform = CATransform3DIdentity;
+    
+    return;
     
     if ( self.isCircle ) {
         CABasicAnimation *morph = [CABasicAnimation animationWithKeyPath:@"path"];
@@ -142,26 +280,19 @@ const CGFloat offset = 5.0f;
     }
     
     
-//    CATransform3D finalPosition = CATransform3DMakeScale(0.1, 0.1, 1.0);
-//    
-//    CABasicAnimation *scaleAnim = [CABasicAnimation animationWithKeyPath:@"transform"];
-//    scaleAnim.fromValue = [NSValue valueWithCATransform3D:CATransform3DIdentity];
-//    scaleAnim.toValue = [NSValue valueWithCATransform3D:finalPosition];
-//    scaleAnim.removedOnCompletion = YES;
-//    scaleAnim.duration = 0.3;
-//    scaleAnim.delegate = self;
-//    scaleAnim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
-//    
-//    
-//    [self.shapeLayer addAnimation:scaleAnim forKey:nil];
-//    self.shapeLayer.transform = finalPosition;
+    
+    
 }
 
 - (void)animationDidStop:(CAAnimation *)theAnimation finished:(BOOL)flag {
 //    [self.shapeLayer removeFromSuperlayer];
-
-
-    
+    if ( CATransform3DIsIdentity(self.shapeLayer.transform)) {
+        CATransform3D lastFrame = CATransform3DMakeScale(0.0f, 0.0f, 1.0); // halfway to 1.0 from 0.75
+        lastFrame = CATransform3DRotate(lastFrame, M_PI, 0, 0, 1);
+        self.shapeLayer.transform = lastFrame;
+    } else {
+        self.shapeLayer.transform = CATransform3DIdentity;
+    }
     
 }
 
